7daf589a4f574378850c0675d05394b5c02588ae
Dominic <dominic.zedler.one@gmail.com>
2018-05-12 22:29:40 +0200

    Changed intentname handling, so user can fork intents now

--- action-domi-toggleFeedbackSound-Signalsound.py
+++ action-domi-toggleFeedbackSound-Signalsound.py
@@ -10,25 +10,27 @@ mqtt_client = mqtt.Client()
 def on_connect(client, userdata, flags, rc):
     client.subscribe("hermes/intent/domi:toggleFeedbackSound")
 
-def toggle_sound(client, userdata, msg):
-    slots = parse_slots(msg)
-    session_id = json.loads(msg.payload.decode("utf-8"))['sessionId']
-    if slots['toggle_state'] == "on":
-        mqtt_client.publish('hermes/feedback/sound/toggleOn', json.dumps({"siteId": "default"}))
-        text = "Der Signalton wurde angeschaltet."
-    else:
-        mqtt_client.publish('hermes/feedback/sound/toggleOff', json.dumps({"siteId": "default"}))
-        text = "Der Signalton wurde ausgeschaltet."
-    mqtt_client.publish('hermes/dialogueManager/endSession',
-                        json.dumps({'text': text, "sessionId": session_id}))
+def on_message(client, userdata, msg):
+    data = json.loads(msg.payload.decode())
+    slots = parse_slots(data)
+    session_id = data['sessionId']
+    user,intentname = data['intent']['intentName'].split(':')
+    if intentname == "toggleFeedbackSound":
+        if slots['toggle_state'] == "on":
+            mqtt_client.publish('hermes/feedback/sound/toggleOn', json.dumps({"siteId": "default"}))
+            text = "Der Signalton wurde angeschaltet."
+        elif if slots['toggle_state'] == "off":
+            mqtt_client.publish('hermes/feedback/sound/toggleOff', json.dumps({"siteId": "default"}))
+            text = "Der Signalton wurde ausgeschaltet."
+        mqtt_client.publish('hermes/dialogueManager/endSession',
+                            json.dumps({'text': text, "sessionId": session_id}))
 
-def parse_slots(msg):
+def parse_slots(data):
     # We extract the slots as a dict
-    data = json.loads(msg.payload.decode("utf-8"))
     return {slot['slotName']: slot['value']['value'] for slot in data['slots']}  # Slotvalue
 
 if __name__ == "__main__":
     mqtt_client.on_connect = on_connect
-    mqtt_client.message_callback_add("hermes/intent/domi:toggleFeedbackSound", toggle_sound)
+    mqtt_client.on_message = on_message
     mqtt_client.connect("localhost", 1883)
     mqtt_client.loop_forever()
